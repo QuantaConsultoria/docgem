51c0a00ed777c7495a99d387982f286fd2442d8b
Fernando Chucre <fernando.chucre@quantaconsultoria.com>
2013-05-07 18:27:15 -0300

    desenhando estrutura basica dos elementos de documentação

--- src/main/resources/templates/docgem.js
+++ src/main/resources/templates/docgem.js
@@ -5,10 +5,10 @@ var drawCharpters = function(charpters) {
 		$("#index").append("<li>"+charpter.id+"<li>");
 		for(var j in charpter.sections) {
 			var section = charpter.sections[j];
-			$.(".charpter[data-id='"+charpter.id+"']" ).find("sections").append($.tmpl( $("#sectionTemplate").text(), section ));
+			$(".charpter[data-id='"+charpter.id+"']" ).find(".sections").append($.tmpl( $("#sectionTemplate").text(), section ));
 			for(var k in section.actions) {
 				var action = section.actions[k];
-				
+				$(".section[data-id='"+section.id+"']" ).find(".actions").append($.tmpl( $("#actionTemplate").text(), action ));
 			}
 		}
 	}
