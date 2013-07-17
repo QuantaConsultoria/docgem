a0335c29d6215b56b6497dfe2edf693c921bab63
Nayalison <Nayalison>
2013-07-17 15:29:59 -0300

    Correção na função que gera o index.

--- src/main/resources/templates/docgem.js
+++ src/main/resources/templates/docgem.js
@@ -2,7 +2,7 @@ var drawChapters = function(chapters) {
 	for (var i in chapters) {
 		var chapter = chapters[i];
 		$.tmpl( $("#chapterTemplate").text(), chapter ).appendTo( "#chapters" );
-		$("#index").append("<li>"+chapter.id+"<li>");
+		$("#index").append("<li>"+chapter.id+"</li>");
 		for(var j in chapter.sections) {
 			var section = chapter.sections[j];
 			$(".chapter[data-id='"+chapter.id+"']" ).find(".sections").append($.tmpl( $("#sectionTemplate").text(), section ));
