95570ddcbced83bc626ac41029ca7bbb88fa217a
Nayalison <Nayalison>
2013-07-22 16:22:58 -0300

    IMplementação da função que habilita os botões.

--- src/main/resources/templates/docgem.js
+++ src/main/resources/templates/docgem.js
@@ -1,3 +1,8 @@
+var Action = {
+		NEXT: 'NEXT',
+		BACK: 'BACK'
+};
+
 var drawChapters = function(chapters) {
 	for (var i in chapters) {
 		var chapter = chapters[i];
@@ -22,6 +27,7 @@ var previousAction = function(e) {
 	e.preventDefault();
 	var lastAction = $("div[data-id='" + sectionId + "']").find(".action.active");
 	var lastIndex = $(lastAction).data("index");
+	validateAction(lastIndex, Action.BACK, sectionId);
 	
 	$(lastAction).removeClass("active");
 	$(lastAction).addClass("disabled");
@@ -29,16 +35,43 @@ var previousAction = function(e) {
 	$("div[data-id='" + sectionId + "']").find(".action[data-index='" + (lastIndex - 1) + "']").removeClass("disabled");
 };
 
+
 var nextAction = function(e) {
 	var sectionId = $(this).data("id");
 	e.preventDefault();
 	var lastAction = $("div[data-id='" + sectionId + "'] .active");
 	var lastIndex = $(lastAction).data("index");
+	validateAction(lastIndex, Action.NEXT, sectionId);
 	
 	$(lastAction).removeClass("active");
 	$(lastAction).addClass("disabled");
 	$("div[data-id='" + sectionId + "']").find(".action[data-index='" + (lastIndex + 1) + "']").addClass("active");
 	$("div[data-id='" + sectionId + "']").find(".action[data-index='" + (lastIndex + 1) + "']").removeClass("disabled");
+};
+
+var validateAction = function(index, action, sectionId) {
+	var qtdActions = $("div[data-id='" + sectionId + "']").find(".action").length;
+	
+	if(action == Action.NEXT) {
+		index = index + 1;
+		if(index > 0) {
+			$("div[data-id='" + sectionId + "']").find(".back-action").removeAttr("disabled");
+		}
+		
+		if(index == qtdActions -1) {
+			$("div[data-id='" + sectionId + "']").find(".next-action").attr("disabled", "disabled");
+		}
+	} else {
+		index = index - 1;
+		
+		if(index < qtdActions -1) {
+			$("div[data-id='" + sectionId + "']").find(".next-action").removeAttr("disabled");
+		}
+		
+		if(index == 0) {
+			$("div[data-id='" + sectionId + "']").find(".back-action").attr("disabled", "disabled");
+		}
+	}
 };
 
 $(function(){
