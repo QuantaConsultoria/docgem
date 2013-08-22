7c9959c4f79835206cca8156f97c9702152bfc25
Nayalison <nayalison@gmail.com>
2013-08-22 18:26:17 +0000

    Separando a responsabilidade de manipular arquivos

--- src/main/java/com/quantaconsultoria/docgem/Documentation.java
+++ src/main/java/com/quantaconsultoria/docgem/Documentation.java
@@ -1,21 +1,12 @@
 package com.quantaconsultoria.docgem;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.nio.channels.FileLock;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
@@ -28,18 +19,19 @@ import com.quantaconsultoria.docgem.bags.ActionBag;
 import com.quantaconsultoria.docgem.bags.ChapterBag;
 import com.quantaconsultoria.docgem.bags.SectionBag;
 import com.quantaconsultoria.docgem.reflections.ReflectionsUtil;
-import com.thoughtworks.xstream.XStream;
 
 public class Documentation {
 
 	private RemoteWebDriver driver;
 	private DocumentationConfiguration configuration;
 	private DocumentationScanner scanner;
+	private FileManager fileManager;
 	
 	private static final Logger LOG = Logger.getLogger(Documentation.class.getName());  
 	
 	
 	protected Documentation() {
+		
 	}
 	
 	public Documentation(RemoteWebDriver driver, DocumentationConfiguration config) {
@@ -48,10 +40,11 @@ public class Documentation {
 		scanner.scan();
 		this.driver = driver;
 		this.configuration = config;
+		fileManager = new FileManager(configuration);
 	}
 
 	public void makeIt() throws IOException {
-		copyResources();
+		fileManager.copyResources();
 		buildJson();
 	}
 
@@ -61,16 +54,15 @@ public class Documentation {
 			Gson gson = new Gson();			
 			String json = gson.toJson(sortedChapters);
 			json = "var chapters = "+json+";";			
-			File data_json = new File(configuration.getTarget(),"data.js");			
-			FileUtils.writeStringToFile(data_json, json);
+			fileManager.saveJson(json);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
 	private List<ChapterBag> mergeWithXmlChaptersBag() throws IOException {
-		List<ChapterBag> finalChapters = getChaptersFromXml();
-		Map<String, ChapterBag> chapters = getCapterFromActionsFile();
+		List<ChapterBag> finalChapters = fileManager.readChapterssXml();
+		Map<String, ChapterBag> chapters = fileManager.readActionsFile();
 		
 		for(ChapterBag xmlChapter : finalChapters) {
 			ChapterBag docChapter = chapters.get(xmlChapter.getId());
@@ -90,111 +82,6 @@ public class Documentation {
 		return finalChapters;
 	}
 
-	private Map<String, ChapterBag> getCapterFromActionsFile() throws IOException {
-		Map<String, ChapterBag> chapters= new HashMap<>();
-		
-		List<String> lines = FileUtils.readLines(new File(configuration.getActionsFile()));
-		for(String line : lines) {
-			String[] parts = line.split("\\;");
-			
-			if(chapters.containsKey(parts[0])) {
-				ChapterBag chapterBag = chapters.get(parts[0]);
-				updateChapterInfo(chapterBag,parts);
-			} else {
-				ChapterBag chapterBag = createChapterFromStringArray(parts);
-				chapters.put(chapterBag.getId(), chapterBag);
-			}
-		}
-		
-		return chapters;
-	}
-
-	private void updateChapterInfo(ChapterBag chapterBag, String[] parts) {
-		SectionBag sectionBag = chapterBag.getSection(parts[1]); 
-		if(sectionBag != null){
-			sectionBag.getActions().add(createActionFomArrayInfo(parts));
-		} else {
-			chapterBag.getSections().add(createSectionFromArrayInfo(parts));
-		}
-	}
-
-	private SectionBag createSectionFromArrayInfo(String[] parts) {
-		SectionBag sectionBag = new SectionBag();
-		sectionBag.setId(parts[1]);
-		sectionBag.setActions(new ArrayList<ActionBag>());
-		sectionBag.getActions().add(createActionFomArrayInfo(parts));
-		
-		return sectionBag;
-	}
-
-	private ActionBag createActionFomArrayInfo(String[] parts) {
-		return new ActionBag(parts[2], parts[3]);
-	}
-
-	private ChapterBag createChapterFromStringArray(String[] parts) {
-		ChapterBag chapterBag = new ChapterBag();
-		chapterBag.setId(parts[0]);
-		chapterBag.setSections(new ArrayList<SectionBag>());
-		
-		SectionBag sectionBag = createSectionFromArrayInfo(parts);
-		chapterBag.getSections().add(sectionBag);
-		
-		return chapterBag;
-	}
-
-	private List<ChapterBag> getChaptersFromXml() throws IOException {
-		InputStream xmlChapters = null;
-		try {
-			XStream xstream = new XStream();
-			xstream.alias("chapters", ArrayList.class);
-			xstream.alias("chapter", ChapterBag.class);
-			xstream.alias("section", SectionBag.class);
-			xstream.alias("action", ActionBag.class);
-			
-			xmlChapters = new FileInputStream(new File(configuration.getChaptersXmlPath()));
-			List<ChapterBag> lista = (List<ChapterBag>)xstream.fromXML(xmlChapters);
-			return lista;
-			
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		} finally {
-			close(xmlChapters);
-		}
-	}
-
-	private void copyResources() {
-		File targetDir = new File(configuration.getTarget());
-		InputStream index = null;
-		InputStream style = null;
-		InputStream docgem_js = null;
-		InputStream jquery = null;
-		InputStream jqueryTmpl = null;
-		
-		try {
-			index = this.getClass().getResourceAsStream("/templates/index.html");
-			style = this.getClass().getResourceAsStream("/templates/style.css");
-			docgem_js = this.getClass().getResourceAsStream("/templates/docgem.js");
-			jquery = this.getClass().getResourceAsStream("/templates/jquery.min.js");
-			jqueryTmpl = this.getClass().getResourceAsStream("/templates/jquery.tmpl.js");
-			
-			
-			FileUtils.copyInputStreamToFile(index, new File(targetDir,"index.html"));
-			FileUtils.copyInputStreamToFile(style, new File(targetDir, "style.css"));
-			FileUtils.copyInputStreamToFile(docgem_js, new File(targetDir, "docgem.js"));
-			FileUtils.copyInputStreamToFile(jquery, new File(targetDir, "jquery.min.js"));
-			FileUtils.copyInputStreamToFile(jqueryTmpl, new File(targetDir, "jquery.tmpl.js"));
-			FileUtils.copyDirectoryToDirectory(new File(this.getClass().getResource("/templates/bootstrap/").getPath()), targetDir);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		} finally {
-			close(index);
-			close(style);
-			close(docgem_js);
-			close(jquery);
-			close(jqueryTmpl);
-		}
-	}
-
 	public void addAction(String text, WebElement element) {
 		try {
 			File imageFile = null;
@@ -206,78 +93,14 @@ public class Documentation {
 			}
 			
 			ImageUtil.circulateElement(imageFile, element);
-			String imageFinalFile = gravarImagem(imageFile);
+			String imageFinalFile = fileManager.saveScreenshot(imageFile);
 			ActionBag action = new ActionBag();
 			action.setText(text);
 			action.setImageFile(imageFinalFile);
 			
-			writeInfoAction(this.getCurrentChapter(), this.getCurrentSection(), action);
+			fileManager.writeInfoAction(this.getCurrentChapter(), this.getCurrentSection(), action);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
-		}
-	}
-	
-	private String gravarImagem(File imagem) throws IOException {
-		
-		Long id =  (long) (Math.random()*1000000000);
-		
-		String destFilePath = "images/"+id+".png"; 
-		
-		File destFile = new File(configuration.getTarget(), destFilePath);
-		
-		if (!destFile.exists()) {
-			FileUtils.copyFile(imagem, destFile);
-			return destFilePath;
-		} else {
-			return gravarImagem(imagem);
-		}
-		
-	}
-	
-	private void writeInfoAction(Chapter chapter, Section section, ActionBag action) throws IOException {
-		File file = new File(configuration.getActionsFile());
-		
-		if (!file.exists() && !file.createNewFile()) {
-			throw new RuntimeException("Can't create the actions file.");
-		}
-		FileLock lock = null;
-		FileOutputStream out = null;
-		try {
-			
-			out = new FileOutputStream(file,true);
-			lock = out.getChannel().lock();
-			
-			String line = String.format("%s;%s;%s;%s%n", chapter.id(),section.id(),action.getText(),action.getImageFile());
-			
-			out.write(line.getBytes(configuration.getEncoding()));
-			
-		} finally {
-			close(lock);
-			close(out);
-		}		
-		
-	}
-
-	private void close(FileOutputStream out) throws IOException {
-		if(out != null) {
-			out.close();
-		}
-	}
-
-	private void close(FileLock lock) throws IOException {
-		if(lock != null) {
-			lock.release();
-			lock.close();
-		}
-	}
-	
-	private void close(InputStream stream) {
-		try {
-			if(stream != null) {
-				stream.close();
-			}
-		} catch (IOException e) {
-			LOG.log(Level.WARNING, "Can't close the InputStream.");
 		}
 	}
 
---
+++ src/main/java/com/quantaconsultoria/docgem/FileManager.java
@@ -0,0 +1,205 @@
+package com.quantaconsultoria.docgem;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.FileLock;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.io.FileUtils;
+
+import com.quantaconsultoria.docgem.annotations.Chapter;
+import com.quantaconsultoria.docgem.annotations.Section;
+import com.quantaconsultoria.docgem.bags.ActionBag;
+import com.quantaconsultoria.docgem.bags.ChapterBag;
+import com.quantaconsultoria.docgem.bags.SectionBag;
+import com.thoughtworks.xstream.XStream;
+
+public class FileManager {
+	
+	private static final Logger LOG = Logger.getLogger(FileManager.class.getName());
+	private DocumentationConfiguration configuration;
+	
+	
+	public FileManager(DocumentationConfiguration configuration) {
+		this.configuration = configuration;
+	}
+
+	public String saveScreenshot(File srcImage) throws IOException {
+		Long id =  (long) (Math.random()*1000000000);
+		String destFilePath = "images/"+id+".png"; 
+		File destFile = new File(configuration.getTarget(), destFilePath);
+		
+		if (!destFile.exists()) {
+			FileUtils.copyFile(srcImage, destFile);
+			return destFilePath;
+		} else {
+			return saveScreenshot(srcImage);
+		}
+	}
+	
+	public void saveJson(String json) throws IOException {
+		File data_json = new File(configuration.getTarget(),"data.js");			
+		FileUtils.writeStringToFile(data_json, json);
+	}
+	public void copyResources() {
+		File targetDir = new File(configuration.getTarget());
+		InputStream index = null;
+		InputStream style = null;
+		InputStream docgem_js = null;
+		InputStream jquery = null;
+		InputStream jqueryTmpl = null;
+		
+		try {
+			index = this.getClass().getResourceAsStream("/templates/index.html");
+			style = this.getClass().getResourceAsStream("/templates/style.css");
+			docgem_js = this.getClass().getResourceAsStream("/templates/docgem.js");
+			jquery = this.getClass().getResourceAsStream("/templates/jquery.min.js");
+			jqueryTmpl = this.getClass().getResourceAsStream("/templates/jquery.tmpl.js");
+			
+			
+			FileUtils.copyInputStreamToFile(index, new File(targetDir,"index.html"));
+			FileUtils.copyInputStreamToFile(style, new File(targetDir, "style.css"));
+			FileUtils.copyInputStreamToFile(docgem_js, new File(targetDir, "docgem.js"));
+			FileUtils.copyInputStreamToFile(jquery, new File(targetDir, "jquery.min.js"));
+			FileUtils.copyInputStreamToFile(jqueryTmpl, new File(targetDir, "jquery.tmpl.js"));
+			FileUtils.copyDirectoryToDirectory(new File(this.getClass().getResource("/templates/bootstrap/").getPath()), targetDir);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			close(index);
+			close(style);
+			close(docgem_js);
+			close(jquery);
+			close(jqueryTmpl);
+		}
+	}
+	
+	public void close(FileOutputStream out) throws IOException {
+		if(out != null) {
+			out.close();
+		}
+	}
+
+	public void close(FileLock lock) throws IOException {
+		if(lock != null) {
+			lock.release();
+			lock.close();
+		}
+	}
+	
+	public void close(InputStream stream) {
+		try {
+			if(stream != null) {
+				stream.close();
+			}
+		} catch (IOException e) {
+			LOG.log(Level.WARNING, "Can't close the InputStream.");
+		}
+	}
+	
+	public void writeInfoAction(Chapter chapter, Section section, ActionBag action) throws IOException {
+		File file = new File(configuration.getActionsFile());
+		
+		if (!file.exists() && !file.createNewFile()) {
+			throw new RuntimeException("Can't create the actions file.");
+		}
+		FileLock lock = null;
+		FileOutputStream out = null;
+		try {
+			
+			out = new FileOutputStream(file,true);
+			lock = out.getChannel().lock();
+			
+			String line = String.format("%s;%s;%s;%s%n", chapter.id(),section.id(),action.getText(),action.getImageFile());
+			
+			out.write(line.getBytes(configuration.getEncoding()));
+			
+		} finally {
+			close(lock);
+			close(out);
+		}		
+		
+	}
+	
+	public List<ChapterBag> readChapterssXml() throws IOException {
+		InputStream xmlChapters = null;
+		try {
+			XStream xstream = new XStream();
+			xstream.alias("chapters", ArrayList.class);
+			xstream.alias("chapter", ChapterBag.class);
+			xstream.alias("section", SectionBag.class);
+			xstream.alias("action", ActionBag.class);
+			
+			xmlChapters = new FileInputStream(new File(configuration.getChaptersXmlPath()));
+			List<ChapterBag> lista = (List<ChapterBag>)xstream.fromXML(xmlChapters);
+			return lista;
+			
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} finally {
+			close(xmlChapters);
+		}
+	}
+	
+	public Map<String, ChapterBag> readActionsFile() throws IOException {
+		Map<String, ChapterBag> chapters= new HashMap<>();
+		
+		List<String> lines = FileUtils.readLines(new File(configuration.getActionsFile()));
+		for(String line : lines) {
+			String[] parts = line.split("\\;");
+			
+			if(chapters.containsKey(parts[0])) {
+				ChapterBag chapterBag = chapters.get(parts[0]);
+				updateChapterInfo(chapterBag,parts);
+			} else {
+				ChapterBag chapterBag = createChapterFromStringArray(parts);
+				chapters.put(chapterBag.getId(), chapterBag);
+			}
+		}
+		
+		return chapters;
+	}
+	
+	private void updateChapterInfo(ChapterBag chapterBag, String[] parts) {
+		SectionBag sectionBag = chapterBag.getSection(parts[1]); 
+		if(sectionBag != null){
+			sectionBag.getActions().add(createActionFomArrayInfo(parts));
+		} else {
+			chapterBag.getSections().add(createSectionFromArrayInfo(parts));
+		}
+	}
+	
+	private SectionBag createSectionFromArrayInfo(String[] parts) {
+		SectionBag sectionBag = new SectionBag();
+		sectionBag.setId(parts[1]);
+		sectionBag.setActions(new ArrayList<ActionBag>());
+		sectionBag.getActions().add(createActionFomArrayInfo(parts));
+		
+		return sectionBag;
+	}
+	
+	private ActionBag createActionFomArrayInfo(String[] parts) {
+		return new ActionBag(parts[2], parts[3]);
+	}
+
+	private ChapterBag createChapterFromStringArray(String[] parts) {
+		ChapterBag chapterBag = new ChapterBag();
+		chapterBag.setId(parts[0]);
+		chapterBag.setSections(new ArrayList<SectionBag>());
+		
+		SectionBag sectionBag = createSectionFromArrayInfo(parts);
+		chapterBag.getSections().add(sectionBag);
+		
+		return chapterBag;
+	}
+
+}
