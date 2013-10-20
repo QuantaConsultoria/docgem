5740628e7d80911ad7bf5fe11c5f6836a05e22ee
Nayalison <nayalison@gmail.com>
2013-10-20 18:26:52 -0300

    Teste para o cenario de falha.

--- src/test/java/com/quantaconsultoria/docgem/test/FileManagerTest.java
+++ src/test/java/com/quantaconsultoria/docgem/test/FileManagerTest.java
@@ -1,16 +1,17 @@
 package com.quantaconsultoria.docgem.test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Matchers;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -21,26 +22,40 @@ import com.quantaconsultoria.docgem.annotations.Chapter;
 
 @Chapter(id = "Capitulo falha")
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(FileUtils.class)
+@PrepareForTest({File.class, FileUtils.class, FileInputStream.class})
 public class FileManagerTest {
 	
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
 	
-	@Before
-	public void startTestStuffs() throws IOException {
+	@Test
+	public void copyResourcesFail() throws IOException {
+		exception.expect(RuntimeException.class);
+		exception.expectMessage("Can't copy resources files.");
+		
 		PowerMockito.mockStatic(FileUtils.class);
 		PowerMockito.doThrow(new IOException("Can't copy resources files.")).when(FileUtils.class);
 		FileUtils.copyInputStreamToFile(Matchers.any(InputStream.class), Matchers.any(File.class));
-	}
-	
-	@Test
-	public void copyResourcesFail() {
-		exception.expect(RuntimeException.class);
-		exception.expectMessage("Can't copy resources files.");
 		
 		DocumentationConfiguration config = new DocumentationConfiguration();
 		FileManager fileManager = new FileManager(config);
 		fileManager.copyResources();
+	}
+	
+	@Test
+	public void readChapterssXmlFail() throws Exception {
+		exception.expect(RuntimeException.class);
+		exception.expectMessage("Can't read XML file.");
+		
+		File file = Mockito.mock(File.class);
+		PowerMockito.whenNew(File.class).withAnyArguments().thenReturn(file);
+		
+		FileInputStream inputStream = Mockito.mock(FileInputStream.class);
+		PowerMockito.whenNew(FileInputStream.class).withAnyArguments().thenReturn(inputStream);
+		
+		DocumentationConfiguration config = new DocumentationConfiguration();
+		config.setChaptersXmlPath("");
+		FileManager fileManager = new FileManager(config);
+		fileManager.readChapterssXml();
 	}
 }
